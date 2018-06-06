aeb6866ee6cdb345d4e9457bb8f6011e375b6cd0
wangjun <wangjun@cloudm.com>
2018-06-06 10:03:34 +0800

    基本配置
    基本配置

---
+++ JWNetworking/JWNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWNetworking
+//
+//  Created by wangjun on 2018/6/6.
+//  Copyright © 2018年 wangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWNetworking/Pods/Target Support Files/Pods-JWNetworking/Pods-JWNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JWNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_JWNetworking
+@end
